c79a6208d2814f93317585aef6c5409a88e57185
Katt4722 <karen.mejiaet12d1@gmail.com>
2025-09-03 18:30:21 -0300

    changuee

--- Calculadora/obj/Debug/net9.0/Calculadora.AssemblyInfo.cs
+++ Calculadora/obj/Debug/net9.0/Calculadora.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f03fb8e019a7618e8c4f0e8dfc19543776e7a1eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2080277bcd8d9ca7df3231b00508fc978ac6e13")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
